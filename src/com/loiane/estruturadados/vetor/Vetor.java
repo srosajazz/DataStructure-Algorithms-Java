3e7ea41d254403a29764f8d4ff78b8814567e3c0
Sergio Rosa <scarlosjazz@gmail.com>
2019-01-18 18:43:10 -0500

    Estrutura de Dados e Algoritmos com Java #02: Vetores e Arrays: Introdução

---
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.loiane.estruturadados.vetor;
+
+public class Vetor {
+	
+	private String[] elementos;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+
+}
